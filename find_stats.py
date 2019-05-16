e2355e016876907e2476c4bc433e9b3835b5b07d
marthagb <marthab16ru.is>
2019-05-16 00:33:17 +0000

    added description

--- find_stats.py
+++ find_stats.py
@@ -2,6 +2,11 @@ from permuta import Perm, PermSet, MeshPatt
 
 
 def find_stats(meshpatt, n):
+	"""Find the number of occurrences of meshpatt in all permutations of length up to n.
+	
+	Returns a list of tuples (p,s) where p is a permutation and s is the number of 
+	occurrences of meshpatt in p.
+	"""
 	all_perms = []
 	for i in range(n+1):
 		all_perms += list(PermSet(i))
